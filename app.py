b9c202825861bdb7bf5b70abb750f2c8902d9591
Saravanan <143484087+23f2002620@users.noreply.github.com>
2025-02-02 10:55:00 +0530

    Updated user registration and admin login functionality, Built Admin dashboard with CRUD operations for subjects, chapters, quizzes, and questions

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from models import db, User, Subject, Chapter, Quiz, Questions, Scores
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, time
 
 app = Flask(__name__)
 
@@ -19,11 +19,9 @@ def setup_admin():
         db.session.commit()
 
 
-
 @app.route("/")
 def home():
     return render_template("home.html")
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -46,15 +44,12 @@ def register():
     return render_template('user_register.html')  
 
 
-
-@app.route('/login', methods = ['GET','POST'])
-def login():
-
+@app.route('/adminlogin', methods = ['GET', 'POST'])
+def adminlogin():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        #ADMIN
         admin_user = User.query.filter_by(Username=username, Role='admin').first()
 
         if admin_user and check_password_hash(admin_user.Password, password):
@@ -63,24 +58,15 @@ def login():
             flash('Admin login successful!')
             return redirect(url_for('admin_dashboard'))  
         else:
-            flash('Invalid credentials. Please try again.')
+            flash('Admin ONLY')
+            return redirect(url_for('/'))
 
-        #USER
-
-
-
+    return render_template('adminlogin.html')
 
 
-
-
-
-
-
-
-
-    
+@app.route('/login', methods = ['GET','POST'])
+def login():
     return render_template('user_login.html')
-
 
 
 @app.route('/admin_dashboard', methods = ['GET','POST'])
@@ -90,13 +76,11 @@ def admin_dashboard():
         subjects = Subject.query.all()
         chapters = Chapter.query.all()
 
-
-        return render_template('admindashboard.html', subjects = subjects)
+        return render_template('admindashboard.html', subjects = subjects, chapters = chapters)
 
     else:
         flash('Access denied. Admins only.')
-        return redirect(url_for('admin_login'))
-
+        return redirect(url_for('login'))
 
 
 @app.route('/createsubject', methods = ['GET','POST'])
@@ -118,7 +102,6 @@ def createsubject():
     return render_template('create_subject.html')
 
 
-
 @app.route('/editsubject/<int:subject_id>', methods = ['GET','POST'])
 def editsubject(subject_id):
 
@@ -138,7 +121,6 @@ def editsubject(subject_id):
     return render_template('edit_subject.html', subject=subject)
 
 
-
 @app.route('/deletesubject/<int:subject_id>', methods = ['GET','POST'])
 def deletesubject(subject_id):
 
@@ -148,13 +130,21 @@ def deletesubject(subject_id):
 
     subject=Subject.query.get_or_404(subject_id)
     chapter = Chapter.query.filter_by(Subject_id=subject_id).all()
+    quiz = Quiz.query.filter_by(Chapter_id=Chapter.Id).all()
+    questions = Questions.query.filter_by(Quiz_id=Quiz.Id).all()
+    scores = Scores.query.filter_by(Quiz_id=Quiz.Id).all()
     for chap in chapter:
         db.session.delete(chap)
+    for q in quiz:
+        db.session.delete(q)
+    for ques in questions:
+        db.session.delete(ques)
+    for score in scores:
+        db.session.delete(score)
     db.session.delete(subject)
     db.session.commit()
     flash("Subject deleted Successfully")
     return redirect(url_for('admin_dashboard'))
-
 
 
 @app.route('/createchapter/<int:subject_id>', methods = ['GET','POST'])
@@ -177,7 +167,6 @@ def createchapter(subject_id):
     return render_template('create_chapter.html')
 
 
-
 @app.route('/editchapter/<int:chapter_id>', methods = ['GET','POST'])
 def editchapter(chapter_id):
 
@@ -197,7 +186,6 @@ def editchapter(chapter_id):
     return render_template('edit_chap.html', chapter=chapter)
 
 
-
 @app.route('/deletechapter/<int:chapter_id>', methods = ['GET','POST'])
 def deletechapter(chapter_id):
 
@@ -206,10 +194,175 @@ def deletechapter(chapter_id):
         return redirect(url_for('login'))
 
     chapter=Chapter.query.get_or_404(chapter_id)
+    quiz = Quiz.query.filter_by(Chapter_id=Chapter.Id).all()
+    questions = Questions.query.filter_by(Quiz_id=Quiz.Id).all()
+    scores = Scores.query.filter_by(Quiz_id=Quiz.Id).all()
+    for q in quiz:
+        db.session.delete(q)
+    for ques in questions:
+        db.session.delete(ques)
+    for score in scores:
+        db.session.delete(score)
     db.session.delete(chapter)
     db.session.commit()
     flash("Chapter deleted Successfully")
     return redirect(url_for('admin_dashboard'))
+
+
+@app.route('/quizdashboard', methods = ['GET','POST'])
+def quizdashboard():
+
+    if 'user_id' in session and session.get('role') == 'admin':
+        quiz = Quiz.query.all()
+        chapter = Chapter.query.all()
+        return render_template('quizdashboard.html', quiz = quiz, chapter = chapter)
+
+    else:
+        flash('Access denied. Admins only.')
+        return redirect(url_for('login'))
+
+
+@app.route('/createquiz', methods =["GET", "POST"])
+def createquiz():
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        chapterid = request.form['chapterid']
+        date = request.form['date']
+        duration = request.form['duration']
+        remarks = request.form['remarks']
+
+        date1 = datetime.strptime(date, '%Y-%m-%d').date()
+        hours, minutes = map(int, duration.split(':'))
+        duration1 = time(hours, minutes)
+
+        quiz = Quiz(Chapter_id = chapterid, dateofquiz = date1, timeduration = duration1, remarks = remarks)
+        db.session.add(quiz)
+        db.session.commit()
+        flash("Quiz Created Successfully")
+        return redirect(url_for('quizdashboard'))
+    return render_template('create_quiz.html')
+
+
+@app.route('/editquiz/<int:quiz_id>', methods = ['GET','POST'])
+def editquiz(quiz_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        return redirect(url_for('login'))
+    
+    quiz = Quiz.query.get_or_404(quiz_id)
+    if request.method == 'POST':
+        dateofquiz = request.form['date']
+        timeduration = request.form['duration']
+        remarks = request.form['remarks']
+
+        date1 = datetime.strptime(dateofquiz, '%Y-%m-%d').date()
+        hours, minutes = map(int, timeduration.split(':'))
+        duration1 = time(hours, minutes)
+
+        quiz.dateofquiz = date1
+        quiz.timeduration = duration1
+        quiz.remarks = remarks
+
+        db.session.commit()
+        flash("Quiz Created Successfully")
+        return redirect(url_for('quizdashboard'))
+    return render_template('editingquiz.html')
+
+
+@app.route('/deletequiz/<int:quiz_id>', methods = ['GET','POST'])
+
+def deletequiz(quiz_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        return redirect(url_for('login'))
+
+    quiz = Quiz.query.get_or_404(quiz_id)
+    questions = Questions.query.filter_by(Quiz_id=Quiz.Id).all()
+    scores = Scores.query.filter_by(Quiz_id=Quiz.Id).all()
+    for ques in questions:
+        db.session.delete(ques)
+    for score in scores:
+        db.session.delete(score)
+    db.session.delete(quiz)
+    db.session.commit()
+    flash("Quiz deleted Successfully")
+    return redirect(url_for('quizdashboard'))
+
+
+@app.route('/quiz/<int:quiz_id>', methods = ['GET','POST'])
+
+def quiz(quiz_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        redirect(url_for('login'))
+
+    else:
+        questions = Questions.query.filter_by(Quiz_id=quiz_id).all()   
+        return render_template('questions.html', quiz_id = quiz_id, questions = questions)
+
+
+@app.route('/createques/<int:quiz_id>', methods = ['GET','POST'])
+def createques(quiz_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        redirect(url_for('login'))
+
+    if request.method == 'POST':
+        question = request.form['question']
+        options = request.form['Options']
+        answer = request.form['answer']
+
+        question = Questions(Quiz_id = quiz_id, Question = question, Options = options, Answer = answer)
+        db.session.add(question)
+        db.session.commit()
+        flash('Question added Successfully')
+        return redirect(url_for('quiz', quiz_id = quiz_id))
+    return render_template('createques.html')
+
+
+@app.route('/editques/<int:ques_id>', methods = ['GET', 'POST'])
+def editques(ques_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        redirect(url_for('login'))
+    
+    question = Questions.query.get_or_404(ques_id)
+    if request.method == 'POST':
+        question1 = request.form['question']
+        options = request.form['Options']
+        answer = request.form['answer']
+
+        question.Question = question1
+        question.Options = options
+        question.Answer = answer
+
+        db.session.commit()
+        flash('Question Edited Successfully')
+        return redirect(url_for('quiz', quiz_id = question.Quiz_id))
+    return render_template('editques.html')
+
+
+@app.route('/deleteques/<int:ques_id>', methods = ["GET","POST"])
+def deleteques(ques_id):
+
+    if 'user_id' not in session and session.get('role') != 'admin':
+        flash('Access denied. Admins only.')
+        redirect(url_for('login'))
+
+    question = Questions.query.get_or_404(ques_id)
+    db.session.delete(question)
+    db.session.commit()
+    flash("Question deleted Successfully")
+    return redirect(url_for('quiz', quiz_id = question.Quiz_id))
 
 
 
